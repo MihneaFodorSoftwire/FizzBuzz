9ab78824a44c78d282d8275c53b47ca448700e22
Mihnea Fodor <Mihnea.Fodor@softwire.com>
2025-07-02 15:43:31 +0100

    Part 2 almost done

--- index.ts
+++ index.ts
@@ -1,13 +1,33 @@
 function fizzbuzz(): void {
     for (let i = 1; i < 100; ++i) {
+        let ok: boolean = false;
+        let output: string = '';
+        let outputB: string = '';
         if (i % 3 == 0) {
-            if (i % 5 == 0) {
-                console.log("FizzBuzz");
-            } else {
-                console.log("Fizz");
-            }
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
+            ok = true;
+            output += "Fizz";
+        }
+        if (i % 5 == 0) {
+            ok = true;
+            outputB += "Buzz";
+        }
+        if (i % 7 == 0) {
+            ok = true;
+            outputB += "Bang";
+        }
+        if (i % 11 == 0) {
+            ok = true;
+            outputB = "Bong";
+        }
+        if (i % 13 == 0) {
+            ok = true;
+            output += "Fezz";
+        }
+        if (i % 17 == 0) {
+            ok = true;
+        }
+        if (ok) {
+            console.log(output + outputB);
         }
     }
 }
